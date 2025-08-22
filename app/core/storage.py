e220caf79f37e4d40726e716a8824d42c22da7b4
Shivandru-terra <shivandru@letsterra.com>
2025-08-22 10:27:14 +0530

    madeApisDynamic

--- app/core/storage.py
+++ app/core/storage.py
@@ -5,39 +5,43 @@ class GCSStorageService:
     def __init__(self, bucket_name: str):
         self.client = storage.Client()
         self.bucket = self.client.bucket(bucket_name)
-        self.base_prefix = "school-game/knowledge-base/"
+        # self.base_prefix = "school-game/"
     
-    def list_files(self, prefix: str = "") -> List[str]:
+    def list_files(self, game_id: str = "" ) -> List[str]:
         try:
-            full_prefix = self.base_prefix
+            full_prefix = f"{game_id}/"
             blobs_iter = self.bucket.list_blobs(prefix=full_prefix)
-            return [b.name for b in blobs_iter if not b.name.endswith('/')]
+            # return [b.name for b in blobs_iter if not b.name.endswith('/')]
+            return [
+                b.name.replace(f"{game_id}/", "", 1)
+                for b in blobs_iter
+                if not b.name.endswith('/')
+            ]
         except Exception as e:
             raise RuntimeError(e)
 
-    def read_file(self, file_name: str) -> str:
+    def read_file(self, file_path: str) -> str:
         try:
-            blob = self.bucket.blob(self.base_prefix + file_name)
+            print("filename from read file", file_path);
+            blob = self.bucket.blob(file_path)
             return blob.download_as_text()
         except Exception as e:
             raise RuntimeError(e)
-    def upload_file(self, file_name: str, file_content: str) -> None:
+    def upload_file(self, file_path: str, file_content: str) -> None:
         try:
-            blob = self.bucket.blob(self.base_prefix + file_name)
+            blob = self.bucket.blob(file_path)
             blob.upload_from_string(file_content)
         except Exception as e:
             raise RuntimeError(e)
-    # def upload_content(self, blob_name: str, content: str) -> None:
-    #     pass
-    def update_file(self, blob_name: str, content: str) -> None:
+    def update_file(self, file_path: str, content: str) -> None:
         try:
-            blob = self.bucket.blob(self.base_prefix + blob_name)
+            blob = self.bucket.blob(file_path)
             blob.upload_from_string(content, content_type="text/plain")
         except Exception as e:
             raise RuntimeError(e)
-    def delete_file(self, blob_name: str) -> None:
+    def delete_file(self, file_path: str) -> None:
         try:
-            blob = self.bucket.blob(self.base_prefix + blob_name)
+            blob = self.bucket.blob(file_path)
             blob.delete()
         except Exception as e:
             raise RuntimeError(e)
